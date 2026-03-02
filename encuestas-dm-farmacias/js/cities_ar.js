a4e3a180e7dc4ae5a67886111876ebe5dc6bba91
rafaelvig <rafaelviglierchio@gmail.com>
2026-03-02 18:54:45 -0300

    Create cities_ar.js

---
+++ encuestas-dm-farmacias/js/cities_ar.js
@@ -0,0 +1,177 @@
+// js/cities_ar.js
+// Listas cortas por provincia (arrancás con las que te importan y vas ampliando)
+window.CITIES_BY_PROVINCE = {
+  "Chaco": [
+    "Resistencia",
+    "Barranqueras",
+    "Presidencia Roque Sáenz Peña",
+    "Villa Ángela",
+    "Charata"
+  ],
+  "Santa Fe": [
+    "Rosario",
+    "Santa Fe",
+    "Rafaela",
+    "Venado Tuerto",
+    "Reconquista"
+  ],
+  "Buenos Aires": [
+    "La Plata",
+    "Mar del Plata",
+    "Bahía Blanca",
+    "San Nicolás de los Arroyos",
+    "Tandil"
+  "Buenos Aires": [
+    "La Plata",
+    "Mar del Plata",
+    "Bahía Blanca",
+    "San Nicolás de los Arroyos",
+    "Tandil"
+  ],
+  "Ciudad Autónoma de Buenos Aires": [
+    "Ciudad Autónoma de Buenos Aires"
+  ],
+  "Catamarca": [
+    "San Fernando del Valle de Catamarca",
+    "Andalgalá",
+    "Belén",
+    "Tinogasta"
+  ],
+  "Chaco": [
+    "Resistencia",
+    "Barranqueras",
+    "Presidencia Roque Sáenz Peña",
+    "Villa Ángela",
+    "Charata"
+  ],
+  "Chubut": [
+    "Rawson",
+    "Comodoro Rivadavia",
+    "Trelew",
+    "Puerto Madryn",
+    "Esquel"
+  ],
+  "Córdoba": [
+    "Córdoba",
+    "Villa Carlos Paz",
+    "Río Cuarto",
+    "San Francisco",
+    "Villa María"
+  ],
+  "Corrientes": [
+    "Corrientes",
+    "Goya",
+    "Paso de los Libres",
+    "Mercedes",
+    "Curuzú Cuatiá"
+  ],
+  "Entre Ríos": [
+    "Paraná",
+    "Concordia",
+    "Gualeguaychú",
+    "Concepción del Uruguay",
+    "Victoria"
+  ],
+  "Formosa": [
+    "Formosa",
+    "Clorinda",
+    "Pirané",
+    "El Colorado"
+  ],
+  "Jujuy": [
+    "San Salvador de Jujuy",
+    "Palpalá",
+    "Perico",
+    "Libertador General San Martín"
+  ],
+  "La Pampa": [
+    "Santa Rosa",
+    "General Pico",
+    "Toay",
+    "Eduardo Castex"
+  ],
+  "La Rioja": [
+    "La Rioja",
+    "Chilecito",
+    "Aimogasta",
+    "Chamical"
+  ],
+  "Mendoza": [
+    "Mendoza",
+    "San Rafael",
+    "Godoy Cruz",
+    "Maipú",
+    "Luján de Cuyo"
+  ],
+  "Misiones": [
+    "Posadas",
+    "Oberá",
+    "Eldorado",
+    "Puerto Iguazú",
+    "Leandro N. Alem"
+  ],
+  "Neuquén": [
+    "Neuquén",
+    "Cutral Co",
+    "Plottier",
+    "Zapala",
+    "San Martín de los Andes"
+  ],
+  "Río Negro": [
+    "Viedma",
+    "General Roca",
+    "San Carlos de Bariloche",
+    "Cipolletti",
+    "Villa Regina"
+  ],
+  "Salta": [
+    "Salta",
+    "Tartagal",
+    "Orán",
+    "Metán",
+    "Cafayate"
+  ],
+  "San Juan": [
+    "San Juan",
+    "Rivadavia",
+    "Rawson",
+    "Chimbas",
+    "Pocito"
+  ],
+  "San Luis": [
+    "San Luis",
+    "Villa Mercedes",
+    "Merlo",
+    "La Punta"
+  ],
+  "Santa Cruz": [
+    "Río Gallegos",
+    "Caleta Olivia",
+    "El Calafate",
+    "Pico Truncado"
+  ],
+  "Santa Fe": [
+    "Rosario",
+    "Santa Fe",
+    "Rafaela",
+    "Venado Tuerto",
+    "Reconquista"
+  ],
+  "Santiago del Estero": [
+    "Santiago del Estero",
+    "La Banda",
+    "Termas de Río Hondo",
+    "Añatuya"
+  ],
+  "Tierra del Fuego": [
+    "Ushuaia",
+    "Río Grande",
+    "Tolhuin"
+  ],
+  "Tucumán": [
+    "San Miguel de Tucumán",
+    "Yerba Buena",
+    "Concepción",
+    "Tafí Viejo"
+  ]
+};
