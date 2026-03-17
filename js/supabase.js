bc58acc581a0f5325db9c95abeed3e678c5637a3
rafaelvig <rafaelviglierchio@gmail.com>
2026-03-17 12:46:03 -0300

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,4 +1,10 @@
 const SUPABASE_URL = "https://vjwxkruczzjnkzfjgrba.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZqd3hrcnVjenpqbmt6ZmpncmJhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzAxMzQ4MzUsImV4cCI6MjA4NTcxMDgzNX0.8y621Ls7hTiva2zihhOXqlAiIo7omcd73-RWO73FYAs";
 
-window.sb = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY)  ;
+window.sb = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
+  auth: {
+    persistSession: true,
+    autoRefreshToken: true,
+    detectSessionInUrl: true
+  }
+});
